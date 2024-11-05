66bdc5abc05ed0e859d077b8cdbcf0d1c2a67c3c
BruPotrony <160125224+BruPotrony@users.noreply.github.com>
2024-11-05 19:37:38 +0100

    Commit connection creada

--- Java/P1-T6-CapaOracle-PotronyLopezBru/src/potrony/bru/SportManagerOracle.java
+++ Java/P1-T6-CapaOracle-PotronyLopezBru/src/potrony/bru/SportManagerOracle.java
@@ -4,7 +4,14 @@
  */
 package potrony.bru;
 
+import java.beans.Statement;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Properties;
 import potrony.bru.SportManager.Categoria;
 import potrony.bru.SportManager.EnumTitular;
 import potrony.bru.SportManager.Equip;
@@ -16,8 +23,42 @@ import potrony.bru.SportManager.Usuari;
  *
  * @author Vago
  */
-public class SportManagerSQL implements SportManagerInferfaceCP {
+public class SportManagerOracle implements SportManagerInferfaceCP {
 
+    private Connection conn;
+    private Statement stmt;
+    
+    public SportManagerOracle() throws GestorSportManagerException {
+        this("connectionBD.properties");
+    }
+    
+    
+    public SportManagerOracle(String nomProperties) throws GestorSportManagerException {
+        
+        String url, user, password;
+        try {
+            Properties p = new Properties();
+            p.load(new FileInputStream(nomProperties));
+            
+            url = p.getProperty("url");
+            user = p.getProperty("user");
+            password = p.getProperty("pwd");
+            
+            if (url==null||url.length()==0|| user==null||user.length()==0|| password == null||password.length()==0){
+                throw new GestorSportManagerException("Hi ha una propietat null o amb longitud 0");
+            }
+            
+            conn = DriverManager.getConnection(url,user,password);
+            conn.setAutoCommit(false);
+            
+        } catch (IOException ex) {
+            throw new GestorSportManagerException("Problemes en recuperar l'arxiu de propietats " + nomProperties, ex);
+        } catch (SQLException ex) {
+            throw new GestorSportManagerException("No es pot establir la connexió.", ex);
+        }
+    }
+
+    
     @Override
     public boolean saveUsuari(Usuari usuari) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
@@ -194,18 +235,36 @@ public class SportManagerSQL implements SportManagerInferfaceCP {
     }
 
     @Override
-    public boolean tancarConnexio() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public boolean tancarConnexio() throws GestorSportManagerException {
+        if (conn != null) {
+            
+            desferCanvis();
+            
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                throw new GestorSportManagerException("Error en tancar la connexió.\n", ex);
+            }
+        }
+        return true;
     }
 
     @Override
-    public void confirmarCanvis() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void confirmarCanvis() throws GestorSportManagerException {
+        try{
+            conn.commit();
+        } catch (SQLException ex) {
+            throw new GestorSportManagerException("Error en fer commit", ex);
+        }
     }
 
     @Override
-    public void desferCanvis() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void desferCanvis() throws GestorSportManagerException {
+        try{
+            conn.rollback();
+        } catch (SQLException ex) {
+            throw new GestorSportManagerException("Error en fer rollback", ex);
+        }
     }
     
 }
--- Java/P1-T6-InterficiePersistencia-PotronyLopezBru/src/potrony/bru/SportManagerInferfaceCP.java
+++ Java/P1-T6-InterficiePersistencia-PotronyLopezBru/src/potrony/bru/SportManagerInferfaceCP.java
@@ -22,37 +22,36 @@ public interface SportManagerInferfaceCP {
     
     /*Per guardar un sol usuari o un List
     retorna un boolea si s'ha guardat o no*/
-    boolean saveUsuari(Usuari usuari);
-    boolean saveUsuaris(List<Usuari> usuaris);
+    boolean saveUsuari(Usuari usuari)throws GestorSportManagerException;
+    boolean saveUsuaris(List<Usuari> usuaris)throws GestorSportManagerException;
     
     /*Per cargar un usuari si sabem el login
     o cargar tots els usuaris que tenim*/
-    Usuari loadUsuari(String login);
-    List<Usuari> loadUsuaris();
+    Usuari loadUsuari(String login)throws GestorSportManagerException;
+    List<Usuari> loadUsuaris()throws GestorSportManagerException;
     
     /*Li passem el login d'un usuari, i un 
     usuari, buscara l'usuari pel login i
     el cambiara*/
-    void modificarUsuari(String login, Usuari usuari);
+    void modificarUsuari(String login, Usuari usuari)throws GestorSportManagerException;
     
     /*Li passem un usuari, el busca pel Login i el
     borra, retorna true si l'ha borrat o false si no*/
-    boolean eliminarUsuari(Usuari usuari);
+    boolean eliminarUsuari(Usuari usuari)throws GestorSportManagerException;
     
     /*Per saber si un usuari esta registrat
     i la contrassenya es correcte*/
-    boolean estaRegistrat (String login, String contrassenya);
+    boolean estaRegistrat (String login, String contrassenya)throws GestorSportManagerException;
     
     /*Per saber si el login ja esta registrat
     ja que ha de ser unic*/
-    boolean loginRepetit(String login);
+    boolean loginRepetit(String login)throws GestorSportManagerException;
     
     /*Encripta i desencripta la contrassenya amb SHA1*/
-    String encriptarContrassenya(String contrassenya);
-    String desencriptarContrassenya(String encriptada);
+    String encriptarContrassenya(String contrassenya)throws GestorSportManagerException;
+    String desencriptarContrassenya(String encriptada)throws GestorSportManagerException;
 
-    @Override
-    public boolean equals(Object obj);
+
     
     
     /********************************TEMPORADA**************************************************/   
@@ -60,18 +59,18 @@ public interface SportManagerInferfaceCP {
     
     /*Per guardar una temporada retorna cert
     o false si s'ha guardat*/
-    boolean saveTemporada(Temporada temporada);
+    boolean saveTemporada(Temporada temporada)throws GestorSportManagerException;
     
     /*Per carregar totes les temporades*/
-    List<Temporada> loadTemporades();
+    List<Temporada> loadTemporades()throws GestorSportManagerException;
     
     /*Eliminar una temporada retorna cert o fals
     si l'ha eliminat correctament*/
-    boolean eliminarTemporada(Temporada temporada);
+    boolean eliminarTemporada(Temporada temporada)throws GestorSportManagerException;
     
     /*Serveix per saber si la temporada ja esta
     inserida per tant no es pot repetir*/
-    boolean temporadaRepetida (Temporada temporada);
+    boolean temporadaRepetida (Temporada temporada)throws GestorSportManagerException;
     
     
     /********************************CATEGORIA**************************************************/  
@@ -79,8 +78,8 @@ public interface SportManagerInferfaceCP {
     
     /*Per cargar una sola categoria sabent nom
     o totes les categories que estan inserides*/
-    Categoria loadCategoria(String nom);
-    List<Categoria> loadCategories();
+    Categoria loadCategoria(String nom)throws GestorSportManagerException;
+    List<Categoria> loadCategories()throws GestorSportManagerException;
     
     
     /********************************Jugadors**************************************************/  
@@ -90,31 +89,31 @@ public interface SportManagerInferfaceCP {
     /*Per guardar un jugador o una llista
     de jugadors, retorna cert o fals si ho 
     guarda correctament*/
-    boolean saveJugador (Jugador jugador);
-    boolean saveJugadors (List<Jugador> jugadors);
+    boolean saveJugador (Jugador jugador)throws GestorSportManagerException;
+    boolean saveJugadors (List<Jugador> jugadors)throws GestorSportManagerException;
     
     /*Cargar tots els jugadors*/
-    List<Jugador> loadJugadors();
+    List<Jugador> loadJugadors()throws GestorSportManagerException;
     
     /*Cargar jugador per idLegal nomes en pot cargar
     un ja que idLegal es unic*/
-    Jugador loadJugadorIdLegal(String idLegal);
+    Jugador loadJugadorIdLegal(String idLegal)throws GestorSportManagerException;
     
     /*Carga una llista dels jugadors que tinguin el 
     nom o cognom que passem*/
-    List<Jugador> loadJugadorNomCognom(String nom, String cognom);
+    List<Jugador> loadJugadorNomCognom(String nom, String cognom)throws GestorSportManagerException;
     
     /*Modifica un jugador passant-li IdLegal i el 
     Jugador retorna cert si ho ha completat*/
-    boolean modificarJugador(String idLegal, Jugador jugador);
+    boolean modificarJugador(String idLegal, Jugador jugador)throws GestorSportManagerException;
     
     /*Busca si un jugador ja esta repetit,
     mitjançant idLegal ja que ha de ser unic*/
-    boolean jugadorRepetit(String idLegal);
+    boolean jugadorRepetit(String idLegal)throws GestorSportManagerException;
     
     /*Elimina un jugador retorna cert o fals
     si ho ha completat*/
-    boolean eliminarJugador (Jugador jugador);
+    boolean eliminarJugador (Jugador jugador)throws GestorSportManagerException;
     
     
     
@@ -125,26 +124,26 @@ public interface SportManagerInferfaceCP {
     /*Per a cargar un equip si sabem el nom
     i la temporada ja que sense la temporada
     pot ser que no sigui unic*/
-    Equip loadEquipNom(String nom, int temporada);
+    Equip loadEquipNom(String nom, int temporada)throws GestorSportManagerException;
     
     /*Per a cargar tots els equips*/
-    List<Equip> loadEquips();
+    List<Equip> loadEquips()throws GestorSportManagerException;
     
     /*Guardar un equip o una llista d'equips
     retorna cert o fals si ho ha guardat
     correctament*/
-    boolean saveEquip(Equip equip);
-    boolean saveEquips(List<Equip> equips);
+    boolean saveEquip(Equip equip)throws GestorSportManagerException;
+    boolean saveEquips(List<Equip> equips)throws GestorSportManagerException;
     
     /*Retorna cert si l'equip esta repetit*/
-    boolean equipRepetit(String nom, int any);
+    boolean equipRepetit(String nom, int any)throws GestorSportManagerException;
     
     /*modificar un equip si sabem el nom i l'any
     retorna cert si s'ha guardat correctament*/
-    boolean modificarEquip (String nom, int any, Equip equip);
+    boolean modificarEquip (String nom, int any, Equip equip)throws GestorSportManagerException;
     
     /*eliminar un Equip, passem l'equip sencer*/
-    boolean eliminarEquip (Equip equip);
+    boolean eliminarEquip (Equip equip)throws GestorSportManagerException;
     
     
     /********************************MEMBRE**************************************************/ 
@@ -152,13 +151,13 @@ public interface SportManagerInferfaceCP {
     
     /*Afegir un jugador a un equip com a titular
     o convidat*/
-    boolean afegirJugadorEquip(EnumTitular titularitat, Equip equip, Jugador jugador);
+    boolean afegirJugadorEquip(EnumTitular titularitat, Equip equip, Jugador jugador)throws GestorSportManagerException;
     
     /*Retorna cert si ja es titular en algun equip*/
-    boolean esTitular(Jugador jugador);
+    boolean esTitular(Jugador jugador)throws GestorSportManagerException;
     
     /*Eliminar jugador d'un equip*/
-    boolean eliminarJugadorEquip(Jugador jugador, Equip equip);
+    boolean eliminarJugadorEquip(Jugador jugador, Equip equip)throws GestorSportManagerException;
     
     
     /********************************OTHERS**************************************************/  
@@ -166,14 +165,14 @@ public interface SportManagerInferfaceCP {
     
     
     /*Serveix per a guardar-ho tot*/
-    void saveAll();
+    void saveAll()throws GestorSportManagerException;
     
     /*Serveix per tancar la connexió amb
     la capa de persistència retorna cert
     si ho aconsegueix*/
-    boolean tancarConnexio();
+    boolean tancarConnexio()throws GestorSportManagerException;
     
     /*Per a fer commit o rollback*/
-    void confirmarCanvis();
-    void desferCanvis();
+    void confirmarCanvis()throws GestorSportManagerException;
+    void desferCanvis()throws GestorSportManagerException;
 }
